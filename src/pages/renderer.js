b34297f94ff915b63341170cf62062f4911368c0
marlon <marlonadilson@gmail.com>
2025-09-26 19:12:43 -0300

    feat: add node-firebird dependency and update package configurations

--- src/pages/renderer.js
+++ src/pages/renderer.js
@@ -1,11 +1,11 @@
 
 
-function fechar(){
+function fechar() {
     api.close();
 }
 
 
-    document.getElementById("btn-selecionar").addEventListener("click", async () => {
+document.getElementById("btn-selecionar").addEventListener("click", async () => {
     const dir = await api.selectDirectory();
     if (dir) {
         document.getElementById("salvar").value = dir;
