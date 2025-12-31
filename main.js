fd04c2683dcc05bb7c01e3f63b9ccfbb9f0d52b0
marlon <marlonadilson@gmail.com>
2025-12-31 14:12:28 -0300

    feat: ajustar identificador do usuário da aplicação e melhorar texto do placeholder no formulário de configuração

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import fs from "fs";
 import { cwd } from 'process';
 
 
+app.setAppUserModelId("com.marlonmarcal.backupdb");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
